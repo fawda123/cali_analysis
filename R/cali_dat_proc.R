26853c978f74167e4076847f723a94e152f8582d
fawda123 <mbafs2012@gmail.com>
2017-08-08 09:55:37 -0500

    moved relevant content from microbio

--- R/cali_dat_proc.R
+++ R/cali_dat_proc.R
@@ -8,21 +8,6 @@ library(FactoMineR)
 # taxonomic abundance data
 # output is list for each taxa level, in long format with abund, site name, wwtp, and treatment (cont)
 
-# municipalities that are names of each spread sheet
-munis <- c('ORco', 'SDci', 'LAco', 'LAci')
-
-# import all taxonomic data for each site from each sheet
-sht_ls <- list()
-for(muni in munis){
-  fl <- list.files('ignore', muni, full.names = T)
-  shts <- excel_sheets(fl) %>% 
-    grep('^CA', ., ignore.case = F, value = T)
-  for(sht in shts){
-    tmp <- read_excel(fl, sheet = sht)
-    sht_ls[[sht]] <- tmp
-  }
-}
-
 # add site meta (from ignore folder)
 site_meta <- data.frame(
   site = paste0('CA', c(1, 2, 3, 4, 7, 8, 9, 11, 5, 10, 12, 6, 13, 14)),
@@ -31,68 +16,55 @@ site_meta <- data.frame(
   stringsAsFactors = F
 )
 
+# taxonomy data for the OTUs
+taxdat <- read_xlsx('ignore/Run4_16S_R1.taxonomy.xlsx', sheet = 'PRIMER') %>% 
+  rename(Domain = Kingdom)
+names(taxdat) <- toupper(names(taxdat))
+
+# OTU abundances by site
+otuabu <- read_xlsx('ignore/Run4_16S_R1.27499.subsample.shared.xlsx', sheet = 'Run4_16S_R1.27499.subsample') %>% 
+  select(-label, -numOtus) %>% 
+  gather('otu', 'abund', -Group) %>% 
+  rename(
+    site = Group, 
+    SPECIES = otu
+    ) %>% 
+  left_join(site_meta, by = 'site') %>% 
+  left_join(taxdat, by = 'SPECIES')
+  
 # empty llist to fill
 phylls <- c('DOMAIN', 'PHYLUM', 'CLASS', 'ORDER', 'FAMILY', 'GENUS', 'SPECIES')
 outs <- vector('list', length = length(phylls))
 names(outs) <- phylls
   
 # create separate list element for each taxanomic level, sites combined at each level
-for(phyll in seq_along(phylls)){
+for(i in seq_along(phylls)){
   
-  out_ls <- list()
-
-  for(sht in names(sht_ls)){
-
-    toget <- phylls[phyll]
-    
-    # find start row for phyll
-    x <- sht_ls[[sht]]
-    x <- as.data.frame(x, stringsAsFactors = F)
-    strt <- grep(paste0('^', toget), x[, 1])
-    
-    # get NA values, min diff from strt is end for phyll
-    if(phyll == length(phylls)){
-      stp <- nrow(x)
-    } else {
-      stp <- grep(paste0('^', phylls[phyll + 1]), x[, 1]) - 1
-    }
-
-    # getcol
-    getcol <- grep('^DOMAIN', x[, 1])
-    getcol <- grep('abundance|count', x[getcol, ], ignore.case = T)[1]
-
-    # get phyll data, format
-    dat <- x[seq(strt, stp), ]
-    dat <- dat[, c(1, getcol)]
-    nms <- dat[1, ]
-    dat <- na.omit(dat[-1,])
-    names(dat) <- nms
-    row.names(dat) <- 1:nrow(dat)
-    names(dat)[1] <- tolower(names(dat)[1])
-    names(dat)[2] <- 'abund'
-    dat[, 1] <- gsub('[[:space:]]*$', '', dat[, 1])
-    
-    out_ls[[sht]] <- dat
-    
-  } 
-
-  # combine sites for the phyll, add metadata
-  comb <- reshape2::melt(out_ls) %>% 
-    rename(site = L1) %>% 
-    mutate(abund = as.numeric(abund)) %>% 
-    left_join(., site_meta, by = 'site')
+  # taxa level to grab
+  togrb <- phylls[i]
   
-  # sometimes there are multiple abundance measures for the same taxa level
-  # these need to be combined
-  names(comb)[1] <- 'tmp'
-  comb <- group_by(comb, tmp, site, wwtp, cont) %>% 
-    summarize(abund = sum(abund)) %>% 
-    ungroup %>% 
-    data.frame
-  names(comb)[1] <- tolower(phylls[phyll])
+  # get relevant columns from otuabu, rename tax level for goruping
+  comb <- otuabu[, c('site', 'wwtp', 'cont', 'abund', togrb)]
+  names(comb)[names(comb) %in% togrb] <- 'grpvar'
+  
+  # group by tax level, sum abundances
+  comb <- group_by(comb, site, wwtp, cont, grpvar) %>% 
+    summarise(abund = sum(abund, na.rm = TRUE)) %>% 
+    select(grpvar, everything()) %>% 
+    arrange(grpvar)
+  
+  # get upper level taxa info for merging
+  hitax <- taxdat[, phylls[1:i], drop = FALSE] %>% 
+    unique
+  names(hitax)[names(hitax) %in% togrb] <- 'grpvar'
 
+  # merge upper level taxa info, format names
+  comb <- left_join(comb, hitax, by = 'grpvar')
+  names(comb)[names(comb) %in% 'grpvar'] <- togrb
+  names(comb) <- tolower(names(comb))
+  
   # append to output
-  outs[[phyll]] <- comb
+  outs[[i]] <- comb
   
 }
 
@@ -249,70 +221,6 @@ scrs <- split(scrs, scrs$wwtp) %>%
 
 envdatpca_muni <- scrs
 save(envdatpca_muni, file = 'data/envdatpca_muni.RData')
-
-##
-# abudat with full taxonomic hierarchy
-
-# # these links are broken 
-# taxurl <- list(
-#   CA2 = 'http://nbc.ece.drexel.edu/results/f22c0edd3f7545d426c17fb098ab3ad6',
-#   CA3 = 'http://nbc.ece.drexel.edu/results/1f19ff149d601ac45d2ad391426b7eb1',
-#   CA4 = 'http://nbc.ece.drexel.edu/results/1b7248a9c4a8f825ba71c34ce2794f53',
-#   CA1 = 'http://nbc.ece.drexel.edu/results/39b5a9b78aefc7117e5f56ee6215a503',
-#   CA7 = 'http://nbc.ece.drexel.edu/results/58f7dc39f300992900f5ac5d617921ec',
-#   CA6 = 'http://nbc.ece.drexel.edu/results/328417aa0d49ab40a3f74956f336cb15',
-#   CA5 = 'http://nbc.ece.drexel.edu/results/b008a5918798fcad483c7c21a30d67b8',
-#   CA8 = 'http://nbc.ece.drexel.edu/results/06d109352045840929b6d45c4b516f28',
-#   CA9 = 'http://nbc.ece.drexel.edu/results/7b6abf151b4d0357be5c3c6525711166',
-#   CA10 = 'http://nbc.ece.drexel.edu/results/d54ea6dc293aa71259e1082a2f5f2d20',
-#   CA11 = 'http://nbc.ece.drexel.edu/results/da303a4c95664d0d088f27998ac82184',
-#   CA12 = 'http://nbc.ece.drexel.edu/results/95e6cb9e641f32a1a0618f1533ec805f',
-#   CA14 = 'http://nbc.ece.drexel.edu/results/d02817c08ea4acad2d57d372fd2ee1b1'
-# )
-# 
-# # have to read lines first, save as csv
-# # direct read with read.table was not working
-# for(site in names(taxurl)){
-#   url <- paste0(taxurl[[site]], '/summarized_results.txt')
-#   dat <- readLines(url)
-#   dat <- gsub('\t', ',', dat)
-#   flnm <- paste0('ignore/', site, '_tax.csv') 
-#   write.csv(dat, flnm, quote = F, row.names = F)
-# }
-
-# reimport csv files
-fls <- list.files('ignore', '^CA.*tax\\.csv', full.names = T)
-tax <- vector('list', length = length(fls))
-sites <- gsub('ignore/|_tax\\.csv', '', fls)
-names(tax) <- fls
-for(fl in fls) tax[[fl]] <- read.csv(fl, header = T, skip = 1, stringsAsFactors = FALSE, na.strings = '')
-
-# combine by site, get uniques
-tax <- do.call('rbind', tax) %>% 
-  select(-QUERY_ID, -BEST_MATCH, -SCORE) %>% 
-  unique
-row.names(tax) <- 1:nrow(tax)
-names(tax) <- tolower(names(tax))
-
-# go through abudat, left merge with taxa given taxonomic level
-data(abudat)
-abudat <- lapply(abudat, function(x){
-  
-  # subset tax by level in abudat (unless domain)
-  mrglev <- names(x)[1]
-  tomrg <- which(names(tax) == mrglev)
-  if(tomrg == ncol(tax)) return(x)
-  tomrg <- tax[, tomrg:ncol(tax)] %>% 
-    unique
-  
-  # join
-  out <- left_join(x, tomrg, by = mrglev)
-  
-  return(out)
-  
-})
-
-save(abudat, file = 'data/abudat.RData')
 
 ######
 # get sites lat/long and euclid dist
